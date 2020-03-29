d2598b151eea3283c8eeec09a28c89dcdfc013b8
imanshafiei <imanshafiei@ymail.com>
2020-03-29 19:22:31 +0430

    bugs fix.

--- ccc_dashboard_panel/settings.py
+++ ccc_dashboard_panel/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
+USE_X_FORWARDED_HOST = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
